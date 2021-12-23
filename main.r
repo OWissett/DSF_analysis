b80590c19eecd8bb606631f271954cddd76ee57f
Oliver Wissett <wissettol@gmail.com>
2021-12-23 10:25:34 +0000

    modified main.r

--- main.r
+++ main.r
@@ -27,6 +27,9 @@ library(dplyr)    # Adds piping and data manipulation functions
 library(readr)    # Allows for easy handling of CSV inputs
 library(reshape2) # Adds the melt function to do some data wrangling.
 library(stringr)  # Adds some nice regex stuff
+library(ggthemes) # Adds nice looking ggplot themes
+library(tidyr)
+library(viridis)
 
 # Load dataset
 DSF_DATAPATH = '../../datasets/Tripeptides_DSF.csv'
@@ -35,7 +38,7 @@ dsf_df <- readr::read_csv(DSF_DATAPATH)
 
 assign_peptide <- function(rowID) {
   if (rowID == 'A')
-    return('none')
+    return('None')
   
   if (rowID == 'B')
     return('KAK')
@@ -58,6 +61,10 @@ assign_condition <- function(colID) {
   }
 }
 
+calculate_gradient <- function(x, y) {
+  return(diff(y)/diff(x))
+}
+
 dsf_tibble <- dsf_df %>%
   select(c(1, seq(2, ncol(.), by = 2))) %>%
   tibble() %>%
@@ -67,18 +74,58 @@ dsf_tibble <- dsf_df %>%
        value.name = 'fluorescence',
        variable.name = 'cellID') %>%
   mutate(rowID = substr(cellID,1,1),
-         colID = str_extract(cellID, '(?<=^[A-Z])[0-9]*')) %>%
+         colID = as.numeric(str_extract(cellID, '(?<=^[A-Z])[0-9]*'))) %>%
   mutate(peptide = sapply(rowID, assign_peptide),
          condition = sapply(colID, assign_condition),
          fluorescence = as.numeric(fluorescence),
          temperature = as.numeric(temperature)) %>%
-  mutate(peptide = factor(peptide, levels = c('none', 'KAK', 'KTK','KGK', 'KFK')),
-         condition = factor(condition, levels = c('HisZ', 'Control')))
+  mutate(peptide = factor(peptide, levels = c('None', 'KAK', 'KTK','KGK', 'KFK')),
+         condition = factor(condition, levels = c('HisZ', 'Control'))) %>%
+  dplyr::filter(colID < 7 & rowID != 'F' & rowID != 'G' & rowID != 'H')
 
+dsf_norm <- dsf_tibble %>% 
+  drop_na() %>%
+  group_by(peptide, condition, cellID) %>%
+  summarise(count = n(),
+            fluorescence.norm = (fluorescence - min(fluorescence))/ (max(fluorescence) - min(fluorescence)),
+            temperature = temperature)
+
+dsf_summary <- dsf_norm %>%
+  group_by(peptide, condition, temperature) %>%
+  summarise(count = n(),
+            fluorescence.mean = mean(fluorescence.norm),
+            fluorescence.se = sd(fluorescence.norm) / sqrt(n()),
+            temperature = temperature) %>%
+  mutate(fluorescence.lower = fluorescence.mean - fluorescence.se,
+         fluorescence.upper = fluorescence.mean + fluorescence.se)
   
 # Plotting time...
 
-ggplot(dsf_tibble, aes(temperature, fluorescence,
+ggplot(dsf_norm %>% filter(condition == 'HisZ'),
+       aes(temperature, fluorescence.norm,
                        colour = peptide,
                        shape = condition)) +
+  geom_point() +
+  theme_classic()
+
+dsf_traces <- ggplot(dsf_summary %>% filter(condition == 'HisZ'),
+                     aes(temperature, fluorescence.mean,
+                        colour = peptide)) +
+  geom_point(size = 2) +
+  theme_hc() +
+  labs(y = 'Relative Fluorescence',
+       x = 'Temperature (°C)',
+       colour = 'Peptide') +
+  theme(
+    axis.title = element_text(face="bold", size = 18),
+    axis.text = element_text(face="bold", size = 12),
+    panel.background = element_rect(fill = 'white'),
+    axis.line = element_line(colour = 'black')
+  ) +
+  scale_color_brewer(palette = 'Set1')
+
+dsf_traces  
+
+ggplot(dsf_summary %>% filter(condition == 'HisZ'), 
+       aes(temperature, gradient, color = peptide)) +
   geom_point()
